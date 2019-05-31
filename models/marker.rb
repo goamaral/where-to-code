ce4f0d76766f536f3749e43996c031ba31d41f38
Goncalo Amaral <goa.docs@gmail.com>
2019-05-31 13:55:15 +0100

    Fix Marker model and converted app template to haml

--- models/marker.rb
+++ models/marker.rb
@@ -2,15 +2,23 @@ class Marker
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :lat, type: Float, presence: :true
-  field :lng, type: Float, presence: true
-  field :name, type: String, presence: true
-  field :opens_at, type: Time, presence: true
-  field :closes_at, type: Time, presence: true
-  field :has_wifi, type: Boolean, presence: true
-  field :has_plugs, type: Boolean, presence: true
-  field :up_votes, type: Integer, presence: true, default: 0
-  field :total_votes, type: Integer, presence: true, default: 0
+  field :lat, type: Float
+  field :lng, type: Float
+  field :name, type: String
+  field :opens_at, type: Time
+  field :closes_at, type: Time
+  field :has_wifi, type: Boolean
+  field :has_plugs, type: Boolean
+  field :up_votes, type: Integer, default: 0
+  field :total_votes, type: Integer, default: 0
+
+  validates :lat, presence: true
+  validates :lng, presence: true
+  validates :name, presence: true
+  validates :opens_at, presence: true
+  validates :closes_at, presence: true
+  validates :has_wifi, presence: true
+  validates :has_plugs, presence: true
 
   def up
     up_votes += 1
